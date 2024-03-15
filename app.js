cb214ccb904143dce9b4bd90e925d50fc5049bdf
AmitAgnihotri <agni.amit81@gmail.com>
2024-03-15 17:04:05 +0530

    Text changes

--- app.js
+++ app.js
@@ -3,7 +3,7 @@ const http = require('http');
 
 const server = http.createServer((req, res) => {
   res.writeHead(200, {'Content-Type': 'text/plain'});
-  res.end('Hello, Amit Agnihotri - Welcome to CICD Pipeline for Node Js App! Happy Learning Using SonarQube - Part 05\n');
+  res.end('Hello, Amit Agnihotri - Welcome to CICD Pipeline for Node Js App! Happy Learning Using SonarQube - Part 06\n');
 });
 
 const port = 3000;
